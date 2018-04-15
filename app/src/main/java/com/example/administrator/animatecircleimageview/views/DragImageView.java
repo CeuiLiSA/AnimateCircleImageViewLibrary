52a1245ff2cd4a01a3a3d855168799f75e8331c6
CeuiLiSA <32817377+CeuiLiSA@users.noreply.github.com>
2018-04-15 15:22:50 +0800

    latest update

--- app/src/main/java/com/example/administrator/animatecircleimageview/views/DragImageView.java
+++ app/src/main/java/com/example/administrator/animatecircleimageview/views/DragImageView.java
@@ -130,7 +130,7 @@ public class DragImageView extends RelativeLayout {
         if (event.getAction() == MotionEvent.ACTION_DOWN && clickInAvatarView(event)) {
             return true;
         }
-        return mDragHelper.shouldInterceptTouchEvent(event);
+        return false;
     }
 
     @Override
